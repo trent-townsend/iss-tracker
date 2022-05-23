c93ffd3ace7b11f13d775e17fcb2e5b77fc63be3
Trent Townsend <trent.townsend2@gmail.com>
2022-05-23 10:18:28 +1000

    Initial commit of starting file

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+from datetime import datetime
+
+MY_LAT = -28.155621
+MY_LONG = 153.472519
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+
+
+
